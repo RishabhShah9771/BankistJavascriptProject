3fece3f9e1df6c561d58b6e33944722bd3889122
RISHABH SHAH <rnshah1997@gmail.com>
2025-03-17 01:11:47 -0400

    FirstTimerUpdate

--- components/loanMoney.js
+++ components/loanMoney.js
@@ -2,26 +2,42 @@ import { btnLoan, inputLoanAmount } from './domElements.js';
 import { currentAccountDetails } from './login.js';
 import { updateUI } from './script.js';
 
+// Function to handle loan money process
 const loanMoney = function () {
+  // Add event listener to the loan button
   btnLoan.addEventListener('click', function (e) {
-    e.preventDefault();
+    e.preventDefault(); // Prevent form submission
+
+    // Get the loan amount from the input field and convert it to a number
     const loanAmount = Number(inputLoanAmount.value);
+
+    // Check if any movement is at least 10% of the requested loan amount
     const loanCondition = currentAccountDetails.movements.some(
       mov => mov >= loanAmount * 0.1
     );
+
+    // If loan amount is positive and condition is met
     if (loanAmount > 0 && loanCondition) {
-      // Add movement
-      currentAccountDetails.movements.push(loanAmount);
+      // Simulate loan processing delay
+      setTimeout(function () {
+        // Add loan amount to movements
+        currentAccountDetails.movements.push(loanAmount);
 
-      //Add Loan Date
-      currentAccountDetails.movementsDates.push(new Date().toISOString());
+        // Add current date to movementsDates
+        currentAccountDetails.movementsDates.push(new Date().toISOString());
 
-      // Update UI
-      updateUI(currentAccountDetails);
+        // Update the user interface with new account details
+        updateUI(currentAccountDetails);
+      }, 2500); // 2.5 seconds delay
+
+      // Clear the input field
       inputLoanAmount.value = '';
     } else {
+      // Alert the user if the loan amount is invalid
       alert('Invalid Loan Amount!!\nYou can get only 10% of Loan!!');
     }
+
+    // Clear the input field
     inputLoanAmount.value = '';
   });
 };
--- components/script.js
+++ components/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Import necessary functions and elements from other modules
 import handleLogin from './login.js';
 import transferMoney from './transferMoney.js';
 import {
@@ -17,26 +18,35 @@ import { formatCurrencyAmount } from './dateOperations.js';
 const calcDisplayBalance = function (accountDetail) {
   // Calculate total income
   const incomeAmount = accountDetail.movements
-    .filter(movement => movement > 0)
-    .reduce((acc, movement) => acc + movement, 0);
-  labelSumIn.textContent = `${incomeAmount}€`;
+    .filter(movement => movement > 0) // Filter deposits
+    .reduce((acc, movement) => acc + movement, 0); // Sum deposits
+  labelSumIn.textContent = formatCurrencyAmount(
+    incomeAmount,
+    accountDetail.currency
+  );
 
   // Calculate total outflow
   const outAmount = accountDetail.movements
-    .filter(movement => movement < 0)
-    .reduce((acc, movement) => acc + movement, 0);
-  labelSumOut.textContent = `${Math.abs(outAmount)}€`;
+    .filter(movement => movement < 0) // Filter withdrawals
+    .reduce((acc, movement) => acc + movement, 0); // Sum withdrawals
+  labelSumOut.textContent = Math.round(
+    formatCurrencyAmount(outAmount, accountDetail.currency)
+  );
 
   // Calculate total interest
   const interestAmount = accountDetail.movements
-    .filter(movement => movement > 0)
-    .map(deposit => (deposit * accountDetail.interestRate) / 100)
-    .reduce((acc, interest) => (interest >= 1 ? acc + interest : acc), 0);
-  labelSumInterest.textContent = `${interestAmount}€`;
+    .filter(movement => movement > 0) // Filter deposits
+    .map(deposit => (deposit * accountDetail.interestRate) / 100) // Calculate interest for each deposit
+    .reduce((acc, interest) => (interest >= 1 ? acc + interest : acc), 0); // Sum interest if it's >= 1
+  labelSumInterest.textContent = formatCurrencyAmount(
+    interestAmount,
+    accountDetail.currency
+  );
 };
 
 // Function to calculate and display total balance of the account
 const calcTotalBalanceOfAccount = function (accountDetail) {
+  // Calculate total balance
   accountDetail.balance = accountDetail.movements.reduce(
     (acc, movement) => acc + movement,
     0
@@ -65,4 +75,5 @@ transferMoney();
 closeAccount();
 loanMoney();
 
+// Export functions for use in other modules
 export { calcDisplayBalance, calcTotalBalanceOfAccount, updateUI };
